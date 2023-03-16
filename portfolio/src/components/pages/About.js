fef660f1d66e1607f2e31da5e59b09d340adaaa6
Raflores10 <120347604+Raflores10@users.noreply.github.com>
2023-03-15 23:27:46 -0700

    update

---
+++ portfolio/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+
+  return (
+    <div>
+      <h1 className='intro'>Hi, I'm Rickie.</h1>
+      <img className='picture' src="" alt="profile"/>
+      <p className='mt-5 text-center text-light bio'>
+      text
+      </p>
+    </div>
+  );
+}
---
+++ portfolio/src/components/pages/Resume.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div className='skills text-center'>
+      <p className='mt-3'>Resume</p>
+      <h2 className='title'>Front End Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>Jquery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <br/>
+      <h2 className='title'>Back End Skills</h2>
+      <ul>
+        <li>API's</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>SQL, Sequelize</li>
+        <li>MongoDB</li>
+        <li>REST</li>
+      </ul>
+    </div>
+  );
+}
